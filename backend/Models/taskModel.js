ed87e0179711e18f3cbc106d41978dec1023d79f
AyushP10 <apandiya10@gmail.com>
2025-03-09 12:42:23 +0530

    9thMarch

--- backend/Models/taskModel.js
+++ backend/Models/taskModel.js
@@ -6,6 +6,7 @@ const { Schema, model } = require('../connection');
 
 const newSchema = new Schema({
     emp_name: {type: String, require: true},
+    emp_email: {type: String, require: true},
     task: {type: String, require:true},
     details: {type: String, require:true},
     deadline: {type: Number, require: true}
